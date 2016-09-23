93aeb10ec3ecaf719895d569341806f20bde1bd0
関　敦 <seki@sekiatsushi-no-MacBook-Air.local>
2016-09-23 17:13:49 +0900

    ミスを修正、ログイン　ログアウトの場合分け

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ before_action :configure_permitted_parameters, if: :devise_controller?
 
 private
 def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :work :avatar])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :work, :avatar])
     end
   end
