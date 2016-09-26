c68dec688a02f710b4deb66ac79a8fbfc0f560ca
関　敦 <seki@sekiatsushi-no-MacBook-Air.local>
2016-09-26 17:26:08 +0900

    prototype,capturedimageモデルをfix, prototypesコントローラをfix

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -8,15 +8,30 @@ class PrototypesController < ApplicationController
 
   def new
     @prototype = Prototype.new
-    @prototype.capturedimages.build
+    @prototype.captured_images.build
   end
 
   def create
     @prototype = @Prototype.create(create_params)
 
+    def destroy
+    @prototype = Prototype.find(params[:id])
+    @prototype.destroy
+    redirect_to :root
+  end
+
   private
+
   def create_params
-    params.require(:prototype).permit(:title, :catch_copy, :concept).merge(user_id: current_user.id)
+    params.require(:prototype).permit(
+      :title,
+      :catch_copy,
+      :concept,
+      captured_images_attributes: [
+      :prototype_id,
+      :photo,
+      :role]
+      ).merge(user_id: current_user.id)
   end
 
 
--- app/models/capturedimage.rb
+++ app/models/capturedimage.rb
@@ -1,3 +1,7 @@
 class Capturedimage < ActiveRecord::Base
   enum role: {main: 0, sub: 1 }
+
+  belongs_to :prototype
+
+  mount_uploader :photo, ImageUploader
 end
