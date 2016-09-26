807863c80142608ee64ed5552496ef371c377bb9
関　敦 <seki@sekiatsushi-no-MacBook-Air.local>
2016-09-26 12:36:30 +0900

    prototypes_controllerをfix

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -7,6 +7,16 @@ class PrototypesController < ApplicationController
   end
 
   def new
+    @prototype = Prototype.new
+    @prototype.capturedimages.build
+  end
+
+  def create
+    @prototype = @Prototype.create(create_params)
+
+  private
+  def create_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept).merge(user_id: current_user.id)
   end
 
 
